9436e518e10ca52c4ec5790174159206c991612d
Chun-Min Chang <chun.m.chang@gmail.com>
2019-12-30 13:26:03 -0800

    Run cargo fmt

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,5 +1,5 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use audio_mixer::{Channel, Mixer};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 use std::any::{Any, TypeId};
 
