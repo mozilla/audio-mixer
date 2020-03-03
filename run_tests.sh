ed16223f1e5391fdd4f964438430a96dde0ba45c
Chun-Min Chang <chun.m.chang@gmail.com>
2020-03-03 09:30:44 -0800

    Reuse compiled crates

--- run_tests.sh
+++ run_tests.sh
@@ -7,16 +7,16 @@ if [ -z "${RUST_BACKTRACE}" ]; then
 fi
 echo "RUST_BACKTRACE is set to ${RUST_BACKTRACE}\n"
 
+# Regular Tests
+# We need to run `cargo clean` before the `cargo test`. Otherwise, we will get a
+# "multiple matching crates for `bitflags`" error when running `test_build_ffi`
+# since `bitflags` crate may be built before (e.g., previous test round) and
+# `cargo test` will build another one again.
+cargo clean
+cargo test --verbose --all-features
+
 # Format check
 cargo fmt -- --check
 
 # Lints check
 cargo clippy --all-targets --all-features -- -D warnings
-
-# Regular Tests
-# We need to run `cargo clean` before the `cargo test`. Otherwise, we will get a
-# "multiple matching crates for `bitflags`" error when running `test_build_ffi`
-# since `cargo clippy` already build a `bitflags` crate and `cargo test` will
-# build another one again.
-cargo clean
-cargo test --verbose --all-features
