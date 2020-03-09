bf2cc728787fe11752c9986b056eb26edd54762d
Chun-Min Chang <chun.m.chang@gmail.com>
2020-03-09 07:59:43 -0700

    Fix Asan build for audio-mixer crate

--- run_sanitizers.sh
+++ run_sanitizers.sh
@@ -20,10 +20,11 @@ echo "RUST_BACKTRACE is set to ${RUST_BACKTRACE}\n"
 
 # {Address, Thread}Sanitizer cannot build with `criterion` crate, so `criterion` will be removed
 # from the `Cargo.toml` temporarily during the sanitizer tests.
-ORIGINAL_MANIFEST="Cargo_ori.toml"
-mv Cargo.toml $ORIGINAL_MANIFEST
+ORIGINAL_MANIFEST="core/Cargo_ori.toml"
+TEST_MANIFEST="core/Cargo.toml"
+mv $TEST_MANIFEST $ORIGINAL_MANIFEST
 # Delete lines that contain a `criterion` from Cargo.toml.
-sed '/criterion/d' $ORIGINAL_MANIFEST > Cargo.toml
+sed '/criterion/d' $ORIGINAL_MANIFEST > $TEST_MANIFEST
 
 sanitizers=("address" "leak" "memory" "thread")
 for san in "${sanitizers[@]}"
@@ -34,7 +35,7 @@ do
     echo "Skip the test that panics. Leaking memory when the program drops out abnormally is ok."
     options="-- --Z unstable-options --exclude-should-panic"
   fi
-  RUSTFLAGS="-Z sanitizer=${san}" cargo test $options
+  RUSTFLAGS="-Z sanitizer=${san}" cargo test -p audio-mixer $options
 done
 
-mv $ORIGINAL_MANIFEST Cargo.toml
+mv $ORIGINAL_MANIFEST $TEST_MANIFEST
